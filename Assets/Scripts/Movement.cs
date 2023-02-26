e3826f03a746e6acc24ce3a929e96fc7921ad72c
Robin <109877121+Plananas@users.noreply.github.com>
2023-02-26 22:45:12 +0000

    Basic Player Control
    Player can move side to side and jump around a basic environment. Still really buggy with movement as player clips through floor and sprite doesn't like flipping.
    Good start for the project.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,26 +1,61 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D body;
+    public Transform  groundChecker;
+    public LayerMask groundlayer;
     public int speed;
+    
+    public Animator animator;
+    public int jumpForce;
+    private bool facingRight = true;
+    //Check if player is touching the ground before they can jump.
+    private bool groundCheck(){
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundChecker.position, 0.2f, groundlayer);
+        if(colliders.Length>0){
+            return true;
+        }
+        return false;
+    } 
 
 
-    // Update is called once per frame
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+
     void Update()
     {
-
+        groundCheck();
+        //Movement Scripts//
         float Horizontal = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(Horizontal * speed, body.velocity.y);
+        //Sets animator up for walking animation.
+        animator.SetFloat("Speed", Math.Abs(body.velocity.x));
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Flip the player sprite.
+        if (Horizontal > 0 && !facingRight)
         {
-            body.AddForce(Vector3.up * 10, ForceMode2D.Impulse);
-
+            Flip();
         }
+        if (Horizontal < 0 && facingRight)
+        {
+            Flip();
+        } 
 
+
+        //Jump if we are on the ground.
+        if (groundCheck() && Input.GetKeyDown(KeyCode.Space)){
+
+            body.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+           
+        }
     }
 }
