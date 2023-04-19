bef9a0ba1ac3e3403d80defdd57f76afce4a9c9c
Robin <109877121+Plananas@users.noreply.github.com>
2023-04-19 23:40:31 +0100

    Crouching and Death
    Player can now crouch however the animation still needs to be added.
    When the player's health hits 0 the movement script will deactivate, Once I have the death animation it will look nicer.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,10 +6,9 @@ using System.Threading;
 public class Player : MonoBehaviour
 {
     public  int          health;
-    public  float          battery;
+    public  float        battery;
     public  float        sanity;
     public  float        ammo; 
-
 
     public  HealthBar    healthBar;
     public  BatteryBar   batteryBar;
@@ -23,6 +22,11 @@ public class Player : MonoBehaviour
     public AudioSource torchOn;
     public AudioSource torchOff;
 
+
+    private bool         dead = false;
+    public  Movement     playerMovement;
+
+
     void Start()
     {    
 
@@ -33,60 +37,57 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        //Updates the bars
-        batteryBar.SetBattery(battery);
-        healthBar.SetHealth(health);
-        
-        //This will neeed to change to trigger if an enemy makes contact with us//
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(2);
-        }
-        if(health <= 0){
-            Death();
-        }
-        //When player presses T, toggle torch.
-        if (Input.GetKeyDown(KeyCode.T) && (battery > 0)){
-            if(torchActive){
+        if(!dead){
+            //Updates the bars
+            batteryBar.SetBattery(battery);
+            healthBar.SetHealth(health);
+            
+            //This will neeed to change to trigger if an enemy makes contact with us//
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TakeDamage(2);
+            }
+            if(health <= 0){
+                Death();
+            }
+            //When player presses T, toggle torch.
+            if (Input.GetKeyDown(KeyCode.T) && (battery > 0)){
+                if(torchActive){
+                    torch.SetActive(false);
+                    torchmesh.SetActive(false);
+                    torchActive = false;
+                    torchOff.Play(0);
+
+                }
+                else{
+                    torch.SetActive(true);
+                    torchmesh.SetActive(true);
+                    torchActive = true;
+                    
+                    torchOn.Play(0);
+
+                }
+
+            }
+            //Turns the torch off when it runs out of power.
+            if(battery <= 0){
+                torchActive = false;
                 torch.SetActive(false);
                 torchmesh.SetActive(false);
-                torchActive = false;
                 torchOff.Play(0);
-
             }
-            else{
-                torch.SetActive(true);
-                torchmesh.SetActive(true);
-                torchActive = true;
-                
-                torchOn.Play(0);
-
+            //Constantly lower the battery when the torch is on.
+            if(torchActive){
+                battery -= 0.5f * Time.deltaTime;
             }
 
-        }
-        //Determines the battery and the torch running out of power.
-        if(battery <= 0){
-            torchActive = false;
-            torch.SetActive(false);
-            torchmesh.SetActive(false);
-            torchOff.Play(0);
-        }
-        if(torchActive){
-            battery -= 0.5f * Time.deltaTime;
+            //Decreases the sanity when the torch is not on//
+            if(!torchActive){
+                sanity -= 0.5f * Time.deltaTime;
+
+            }
         }
 
-        //We will do this when running//
-        //running not implemented just yet//
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            UseBattery(2);
-        }
-
-        //Decreases the sanity when the torch is not on//
-        if(!torchActive){
-            sanity -= 0.5f * Time.deltaTime;
-
-        }
     }
 
     void TakeDamage(int damage)
@@ -102,9 +103,10 @@ public class Player : MonoBehaviour
 
     //This will play the death animation//
     void Death(){
-        
-    }
+        //Deactivate the player movement//
+        playerMovement.enabled = false;
+        dead = true;
 
-     
+    }
 }
 
